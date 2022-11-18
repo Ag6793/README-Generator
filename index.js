abb5bc78f9a85c4afa9b2b734df8a9bbd28c1aaf
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2022-11-17 23:02:01 -0600

    Got the program to work and now working on getting the badges to show up

--- index.js
+++ index.js
@@ -1,90 +1,26 @@
-// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-
-//GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-
-
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const generateMarkdown = require('generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-const generateREADME = ({ project_title, description, technologies, installation, usage, credits, year, fullname}) =>
-    `# ${project_title}
+const generateREADME = [
 
-## Description
-${description}
-
-## Technologies
-${technologies}
-
-## Installation
-${installation}
-
-## Usage
-${usage}
-
-## Credits
-${credits}
-
-## License
-
-MIT License
-
-Copyright (c) [${year}] [${fullname}]
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-## Badges
-/github/license/:user/:repo`;
-
-inquirer 
-    .prompt([
     {
         type: 'input',
-        name: 'project_title',
+        name: 'title',
         message: 'What is the project title?',
     },
-    //What was your motivation?
-    //Why did you build this project?
-    //What problem does it solve?
-    //what did you learn?
-    //what makes your project stand out?
-    //This should include what the app is for, how to use the app, how to install it, how to report issues, and how to make contributions
     {
         type: 'input',
         name: 'description',
         message: 'What is the description for the project?',
     }, 
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What type of license do you want?',
+        choices: ['MIT','APACHE 2.0', 'BSD 2', 'None']
+    },
     {
         type: 'input',
         name: 'technologies',
@@ -104,26 +40,21 @@ inquirer
         type: 'input',
         name: 'credits',
         message: 'List your collaborators and links to their Github profiles',
-    },
-    {
-        type: 'input',
-        name: 'year',
-        message: 'What is the current year?',
-    },
-    {
-        type: 'input',
-        name: 'fullname',
-        message: 'What is your fullname?',
-    },
+    }, 
 
+]
+
+function writeToFile (name, data) {
+    return fs.writeFileSync (name, data)
+}
+
+function app() {
+
+inquirer 
+    .prompt(generateREADME)
+    .then((data) => writeToFile('README.md', generateMarkdown(data))) 
+    .then (() => console.log('Successfully created README.md!'))
+    .catch((err) => console.log(err));
     
-])
-    .then((answers) => {
-        const READcontent = generateREADME(answers);
-
-        fs.writeFile('README.md', READcontent, (err) =>
-            err ? console.log(err) : console.log('Successfully created README.md!')
-        );
-});
-
-
+}
+app();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,23 +5,63 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
+    if (license !== 'None') {
+      return `![GitHub license](https://img.shields.io/badge/license-${license}-red.svg)`
+    } 
+    return ''
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `\n* [license](#license)\n`}
+    else if (license === 'MIT') {
+      return `\n* [license](https://choosealicense.com/licenses/mit/)\n`
+    } else if (license === 'APACHE 2.0') {
+      return `\n* [license](https://choosealicense.com/licenses/apache-2.0/)\n`
+    } else if (license === 'BSD 2') {
+      return `\n* [license](https://opensource.org/licenses/BSD-2-Clause)\n`
+    }
+
+    return ''
+}
+  
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return ('')
+  if (license !== 'None') {
+    return `## License`
+  }
+  return ''
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Technologies
+${data.technologies}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Credits
+${data.credits}
+
+${renderLicenseLink(data.license)}
+
+${renderLicenseSection(data.license)}
 `;
 }
 
